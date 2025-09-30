7dbf63ab8f58080ed7ee495b746cfa75bca95cd8
BurstRange <113287175+BrRange@users.noreply.github.com>
2025-09-30 12:27:05 -0300

    100% working

--- GUI/sdlFrame.c
+++ GUI/sdlFrame.c
@@ -1,31 +1,30 @@
 #include "sdlFrame.h"
 
-void KeyboardHandler_pressKey(KeyboardHandler *kbH, SDL_Keycode key){
-  if(kbH->used >= 6) return;
-  for(Uint32 i = 0; i < kbH->used; ++i){
-    if(kbH->keys[i] == key) return;
-    if(!kbH->keys[i]){
-      kbH->keys[i] = key;
+void KeyboardHandler_pressKey(KeyboardHandler *kbH, SDL_Keycode key) {
+  if (kbH->used >= sizeof(kbH->keys) / sizeof(*kbH->keys)) return;
+  for (Uint32 i = 0; i < kbH->used; ++i) {
+    if (kbH->keys[i] == key) return;
+  }
+  kbH->keys[kbH->used++] = key;
+}
+
+
+void KeyboardHandler_releaseKey(KeyboardHandler *kbH, SDL_Keycode key) {
+  for (Uint32 i = 0; i < kbH->used; ++i) {
+    if (kbH->keys[i] == key) {
+      for (Uint32 j = i; j < kbH->used - 1; ++j)
+        kbH->keys[j] = kbH->keys[j + 1];
+      kbH->used--;
+      kbH->keys[kbH->used] = 0;
       return;
     }
   }
-  kbH->keys[kbH->used] = key;
-  ++kbH->used;
 }
-void KeyboardHandler_releaseKey(KeyboardHandler *kbH, SDL_Keycode key){
-  if(!key) return;
-  for(Uint32 i = 0; i < kbH->used; ++i)
-  if(kbH->keys[i] == key){
-    kbH->keys[i] = 0;
-    if(i == kbH->used)
-    while(kbH->used && !kbH->keys[kbH->used])
-      --kbH->used;
-    return;
-  };
-}
+
 void KeyboardHandler_clear(KeyboardHandler *kbH){
   *kbH = (KeyboardHandler){0};
 }
+
 bool KeyboardHandler_hasKey(KeyboardHandler *kbH, SDL_Keycode key){
   for(Uint32 i = 0; i < sizeof(kbH->keys) / sizeof(kbH->keys[0]); ++i)
   if(kbH->keys[i] == key) return true;
@@ -35,27 +34,33 @@ bool KeyboardHandler_hasKey(KeyboardHandler *kbH, SDL_Keycode key){
 void MouseHandler_setPos(MouseHandler *mouseH, float x, float y){
   mouseH->pos = (SDL_FPoint){.x = x, .y = y};
 }
+
 void MouseHandler_move(MouseHandler *mouseH, float dx, float dy){
   mouseH->move = (SDL_FPoint){.x = dx, .y = dy};
 }
+
 SDL_FPoint MouseHandler_getPos(MouseHandler *mouseH){
   return mouseH->pos;
 }
+
 SDL_FPoint MouseHandler_getMovement(MouseHandler *mouseH){
   SDL_FPoint ret = mouseH->move;
   mouseH->move = (SDL_FPoint){0};
   return ret;
 }
+
 void MouseHandler_pressButton(MouseHandler *mouseH, Uint8 button){
   SDL_assert(button > 0 && button <= 8);
   button = 1 << (button - 1);
   mouseH->down |= button;
 }
+
 void MouseHandler_releaseButton(MouseHandler *mouseH, Uint8 button){
   SDL_assert(button > 0 && button <= 8);
   button = 1 << (button - 1);
   mouseH->up |= button;
 }
+
 Uint8 MouseHandler_hasButton(MouseHandler *mouseH, Uint8 button){
   if(mouseH->down & button){
     if(mouseH->up & button){
@@ -67,14 +72,17 @@ Uint8 MouseHandler_hasButton(MouseHandler *mouseH, Uint8 button){
   }
   return MOUSE_NONE;
 }
+
 void MouseHandler_scroll(MouseHandler *mouseH, float scroll){
   mouseH->scroll = scroll;
 }
+
 float MouseHandler_getScroll(MouseHandler *mouseH){
   Sint16 scroll = mouseH->scroll;
   mouseH->scroll = 0.f;
   return scroll;
 }
+
 void MouseHandler_clear(MouseHandler *mouseH){
   *mouseH = (MouseHandler){.pos = mouseH->pos};
 }
@@ -89,6 +97,7 @@ MenuState MenuState_new(MenuFunc fnTick, MenuFunc fnRender, KeyboardHandler *key
   };
   return menu;
 }
+
 void MenuState_destroy(MenuState *menu){
   Arena_destroy(&menu->memory);
   *menu = (MenuState){0};
@@ -98,6 +107,7 @@ bool FPoint_equal(SDL_FPoint *a, SDL_FPoint *b){
   Uint64 *longA = (void*)a, *longB = (void*)b;
   return *longA == *longB;
 }
+
 void FPoint_copy(SDL_FPoint *dest, SDL_FPoint *src){
   Uint64 *longDest = (void*)dest, *longSrc = (void*)src;
   *longDest = *longSrc;
